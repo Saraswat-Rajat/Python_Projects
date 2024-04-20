f04a920cdddbdf0e5508a9d1df9bcbf0103cf896
Silver <156155046+Saraswat-Rajat@users.noreply.github.com>
2024-04-20 15:03:21 +0530

    Add files via upload

---
+++ Day 1/main.py
@@ -0,0 +1,38 @@
+print("1. Mix 500g of Flour, 10g of Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+
+print("--------------------------------------------------")
+print('''1. Mix 500g of Flour, 10g of Yeast and 300ml Water in a bowl.
+2. Knead the dough for 10 minutes.
+3. Add 3g of Salt.
+4. Leave to rise for 2 hours.
+5. Bake at 200 degrees C for 30 minutes.''')
+
+print("-------------------------------------------")
+
+print('''She said: "Hello" and then left.''')
+
+print("-------------------------------------------")
+
+print("She said: \"Hello\" and then left.")
+
+
+print("-------------------------------------------")
+
+print("Rajat" + " " + "Saraswat")
+
+print("-------------------------------------------")
+
+name = input("What is your name ? ")
+
+print("Hello" + " " + name)
+
+
+print("-----------------------------------------")
+
+num = str(len(input("write your first name ")))
+print("there are " + num + " characters")
